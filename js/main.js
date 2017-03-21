b5e45ae0bb88ac8d56599fd50a6a4e97c2372ea6
luoyu <272864318@qq.com>
2017-03-21 20:05:44 +0800

    Vue的数据绑定、循环、过滤器等

--- js/main.js
+++ js/main.js
@@ -11,3 +11,33 @@
     win.addEventListener(resizeEvt, recalc, false);//resize
     doc.addEventListener('DOMContentLoaded', recalc, false);//reload
 })(document, window);
+
+var vm = new Vue({
+	el:"#app",
+	data: {
+		title: "一个简单的Vue.js demo",
+		text: "尾部",
+		productList: []
+	},
+	// 过滤器
+	filters: {
+		formatMoney: function(value){
+			return value.toFixed(2);
+		}
+	},
+	mounted: function(){
+		this.$nextTick(function(){
+			//以下两种写法均可
+			this.listView();
+//			vm.listView();
+		});
+	},
+	methods: {
+		listView: function () {
+			let _this = this;
+			this.$http.get("data/list.json", {"id": 123}).then(res => {
+				this.productList = res.body.result.list;
+			})
+		}
+	}
+});
