067b418bb8cfa856e0155c87b8de5cbb2b903f9c
luoyu <272864318@qq.com>
2017-03-24 00:00:31 +0800

    新增付款页面

---
+++ shopping_cart/js/address.js
@@ -0,0 +1,32 @@
+var vm = new Vue({
+	el: ".container",
+	data: {
+		limitNum: 3,
+		addressList: []
+	},
+	mounted: function(){
+		this.$nextTick(function(){
+			this.getAddressList();
+		});
+	},
+	computed: {
+		filterAddress: function() {
+			return this.addressList.slice(0,this.limitNum);
+		}
+	},
+	methods: {
+		getAddressList: function () {
+			var _this = this;
+			this.$http.get("data/address.json").then( function(response) {
+				var res = response.data;
+				if(res.status == "1") {
+					
+					_this.addressList = res.result;
+				}
+			})
+		},
+		loadMore: function(){
+			this.limitNum = this.addressList.length;
+		}
+	}
+});
